cabea4b070b83e61b6129336405a59a334574d22
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-02-22 22:22:00 -0600

    Usando Bucle For

---
+++ For/For/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int Vinicial, Vfinal;
+Console.WriteLine("Ingrese primer valor");
+Vinicial = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese segundo valor");
+Vfinal = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Los numeros pares entre estos valores {Vinicial} - {Vfinal}  son:");
+
+for (int i = Vinicial; i <= Vfinal; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
